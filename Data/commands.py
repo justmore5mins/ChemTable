0d2fa4d0b8e5316d26bd54babea8d60d85587469
justmore5mins <standby-nexuses0m@icloud.com>
2023-04-22 20:44:05 +0800

    add

--- Data/commands.py
+++ Data/commands.py
@@ -1,29 +1,30 @@
-from json import dump,dumps,load
+from json import dump,load,JSONDecoder
 from Errors.commanderror import *
 
 filepath = "/Users/AustinYu/Library/CloudStorage/GoogleDrive-tno062789@gmail.com/我的雲端硬碟/python/ChemTable/Data/elems.json"
 
-def addElements(Element_Symbol:str,Element_English_name:str,Atom_Number:int, Element_Characteristics:str = "",Element_Characteristics_Chinese:str = ""):
-    atom_number = str(Atom_Number)
-    
-    if Element_Characteristics == "":
-        Element_Characteristics:None
-    
-    if Element_Characteristics_Chinese == "":
-        Element_Characteristics_Chinese:None
+def addElements(Element_Symbol:str,Element_English_name:str,Atom_Number:int,Valence:int,Atom_mass: float, Element_Characteristics:str = ""):
   
-    element_data = {Element_Symbol:{"English name":Element_English_name,"Atom number":atom_number,"Element Characteristics English":Element_Characteristics,"Element Characteristics Chinese":Element_Characteristics_Chinese}}
-    with open(filepath,"a+",encoding="utf-8") as filewrite:
-        data = filewrite.read()
-        data = str(data)
-        data = data[:-2]
-        data = data+ ","
-        filewrite.truncate(0)
-        filewrite.write("\n")
-
+    if Atom_mass > 0:
+        pass
+    else:
+        raise ValueError("Mass con't be -")
+  
+    element_data = {Element_Symbol:{"English name":Element_English_name,"Atom number":Atom_Number,"Valence":Valence,"Atom Mass":Atom_mass,"Element Characteristics English":Element_Characteristics}}
+    
+    with open(filepath,"r",encoding="utf-8") as file:
+        data = load(file)
         
-    with open(filepath,"a+",encoding="utf-8") as file:
-        dump(element_data,file,indent=2)
-
-#addElements("H","hydrogen",2)
-addElements("He","Helium","2")
+        data.update(element_data)
+        
+    with open(filepath,"w",encoding="utf-8") as filewrite:
+        dump(data,filewrite,indent=2)
+   
+while True:
+    Element_Symbol = input("請輸入元素符號: ")
+    Element_English_name = input("請輸入元素的英文名稱: ")
+    Atom_Number = int(input("請輸入原子序: "))
+    Valence = int(input("請輸入價數: "))
+    Atom_mass = float(input("請輸入原子質量: "))
+    addElements(Element_Symbol=Element_Symbol,Element_English_name=Element_English_name,Atom_Number=Atom_Number,Valence=Valence,Atom_mass=Atom_mass)
+    print('\n')
--- test/test.py
+++ test/test.py
@@ -1,2 +1,4 @@
-with open("/Users/AustinYu/Library/CloudStorage/GoogleDrive-tno062789@gmail.com/我的雲端硬碟/python/ChemTable/test/test.json", 'w') as file:
-    file.truncate(0)
+from json import JSONDecoder
+
+
+thing = JSONDecoder().decode()
